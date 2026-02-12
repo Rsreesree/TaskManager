32174650ccb3c12ca22beb6efb20bc9fa91aa39c
Sreejith <146423662+Rsreesree@users.noreply.github.com>
2026-02-12 19:38:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -342,9 +342,9 @@ def get_stats():
     return jsonify(stats)
 
 # --------------------- Auto Open Browser ---------------------
-def open_browser():
-    time.sleep(1)  # wait for Flask server to start
-    webbrowser.open_new("http://127.0.0.1:5000/")
+#def open_browser():
+    #time.sleep(1)  # wait for Flask server to start
+    #webbrowser.open_new("http://127.0.0.1:5000/")
 
 if __name__ == "__main__":
     threading.Thread(target=open_browser).start()
